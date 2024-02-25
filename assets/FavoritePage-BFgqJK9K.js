0658bb980104c94bba8444037b5d1760186e5031
Pavel Nedoshyvko <plight83@gmail.com>
2024-02-25 18:28:18 +0200

    Updates

---
+++ assets/FavoritePage-BFgqJK9K.js
@@ -0,0 +1,1 @@
+import{j as e,L as r}from"./index-B8jotsTm.js";const a=()=>e.jsxs(e.Fragment,{children:[e.jsx("h1",{children:"FavoritePage"}),e.jsx(r,{to:"/",children:"To Home Page"})]});export{a as default};
